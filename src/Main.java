7529f7a05edde3bb49805ff0bfe9957a23bbb91b
Eduarda Rocha <rochaeduarda98133@gmail.com>
2024-12-01 14:54:07 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+      byte idade = 25;
+
+    if(idade > 22.50){
+        System.out.println("maior");
+    } else if (idade == 22){
+        System.out.println("Igual!");
+    } 
+    else {
+        System.out.println("menor");
+    }
+    }
+}
